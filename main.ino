80520725252b32e197353cc8b560a864bfc55ffd
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2023-02-21 15:13:35 +0100

    Update main.ino

--- main.ino
+++ main.ino
@@ -2,109 +2,65 @@
 #include <ESPmDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+#include <HTTPClient.h>
 
 #define buzzer_pin 33
 #define photoresistore_red_pin 32
 #define photoresistore_blu_pin 35
+#define soglia 2000
 
-const char* ssid = "CEUR Mollino";
-const char* password = "";
+const char* ssid = "OPPO A52";
+const char* password = "internet";
 
-void OTAupload();
+String ServerName = "https://studyroompoli.altervista.org/balilla/getdata.php?token=MullinB&goal=";
+
 void checkgoal_red();
 void checkgoal_blu();
+void checkconnection();
+void httpget(int team);
 
 void setup() {
-  OTAupload();
+  checkconnection();
   pinMode(buzzer_pin,OUTPUT);
-  Serial.begin(115200);
 }
 
 void loop() {
-  ArduinoOTA.handle();
   checkgoal_red();
   checkgoal_blu();
+  delay(1);
 }
 
-void OTAupload(){
-  //Serial.begin(115200);
-  //Serial.println("Booting");
+void checkconnection(){
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    //Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }
-  // Port defaults to 3232
-  // ArduinoOTA.setPort(3232);
-  // Hostname defaults to esp3232-[MAC]
-  // ArduinoOTA.setHostname("myesp32");
-  // No authentication by default
-  // ArduinoOTA.setPassword("admin");
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-  ArduinoOTA
-  .onStart([]() {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH)
-        type = "sketch";
-      else // U_SPIFFS
-        type = "filesystem";
-
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      //Serial.println("Start updating " + type);
-    });
-    //.onEnd([]() {
-    //Serial.println("\nEnd");
-    //})
-    //.onProgress([](unsigned int progress, unsigned int total) {
-    //Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    //})
-    /*.onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    });*/
-  ArduinoOTA.begin();
-  //Serial.println("Ready");
-  //Serial.print("IP address: ");
-  //Serial.println(WiFi.localIP());
+  while (WiFi.status() != WL_CONNECTED) delay(1000);  
 }
 
 void checkgoal_red(){
   int analogValue = analogRead(photoresistore_red_pin);
- 
-  if(analogValue<2000){
+  if(analogValue<soglia){
     digitalWrite(buzzer_pin,HIGH);
-    //Serial.print(analogValue);
-    delay(750);
+    httpget(0);
     digitalWrite(buzzer_pin,LOW);
-    delay(750);
   }
-  Serial.print("Red:");
-  Serial.print(analogValue);
-  Serial.print('\n');
-  delay(30);
+  delay(1);
 }
 
 void checkgoal_blu(){
   int analogValue = analogRead(photoresistore_blu_pin);
- 
-  if(analogValue<2000){
+  if(analogValue<soglia){
     digitalWrite(buzzer_pin,HIGH);
-    //Serial.print(analogValue);
-    delay(750);
+    httpget(1);
     digitalWrite(buzzer_pin,LOW);
-    delay(750);
+  }  
+}
+
+void httpget(int team){
+  if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
+    HTTPClient http;
+    String NameRequest = ServerName + String(team);
+    http.begin(NameRequest); //Specify the URL
+    int httpCode = http.GET();                                        //Make the requesT
+    http.end(); //Free the resources
   }
-  
-  Serial.print("Blu:");
-  Serial.print(analogValue);
-  Serial.print('\n');
-  delay(30);
 }
